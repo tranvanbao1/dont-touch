02fb6c2482197b96cddaad1f4491d79b26b64227
Bảo Trần <tranbao@MacBook-Pro-cua-Tran.local>
2021-10-06 21:02:25 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import './App.css';
+import React, {useEffect, useRef} from 'react';
+import {Howl, Howler} from 'howler';
+import '@tensorflow/tfjs-backend-webgl';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import soundURL from './assets/hey_sondn.mp3'
+import { initNotifications, notify } from '@mycv/f8-notification';
+
+var sound = new Howl({
+  src: [soundURL]
+});
+
+
+const NOT_TOUCH_LABEL = "not_touch";
+const TOUCHED_LABEL = "touched";
+const TRAINING_TIME = 50;
+const TOUCH_CONFIDENT= 0.8;
+
+function App() {
+  const video = useRef();
+  const classifier = useRef();
+  const canPlayAudio = useRef(true);
+  const mobilenetModule = useRef();
+
+  const init = async () => {
+    console.log('init...');
+    await setupCamera();
+    console.log('setup camera success!');
+
+    classifier.current = knnClassifier.create();
+
+    mobilenetModule.current = await mobilenet.load();
+
+    console.log('setup done!');
+    console.log('Không chạm tay vào mặt và bấm Train 1 ');
+
+    initNotifications({ cooldown: 3000 });
+  }
+
+  const setupCamera = ()=> {
+    return new Promise((resolve,reject) => {
+      navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+
+      if(navigator.getUserMedia) {
+        navigator.getUserMedia(
+          {video: true},
+          stream=> {
+            video.current.srcObject = stream;
+            video.current.addEventListener('loadeddata', resolve)
+          },
+          error => reject(error)
+        );
+      }else {
+        reject();
+      }
+    })
+  }
+
+  const train = async label=> {
+    console.log(`[${label}] ahhii`);
+    for(let i = 0;i<TRAINING_TIME; ++i){
+      console.log(`Progress ${parseInt((i+1) / TRAINING_TIME * 100)}%`);
+      await training(label); 
+    }
+  }
+
+  const training= label=> {
+    return new Promise(async resolve=> {
+      const embedding = mobilenetModule.current.infer(
+        video.current,
+        true
+      );
+      classifier.current.addExample(embedding,label);
+      await sleep(100);
+      resolve();
+    })
+  }
+
+  const run = async ()=> {
+    const embedding = mobilenetModule.current.infer(
+      video.current,
+      true
+    );
+    const result = await classifier.current.predictClass(embedding);
+    if(result.label === TOUCHED_LABEL && result.confidences[result.label]> TOUCH_CONFIDENT){
+      console.log('Touched');
+      if(canPlayAudio.current){
+        canPlayAudio.current=false;
+        sound.play();
+      }
+      notify('Bỏ tay ra bạn ơi', { body: 'Bạn vừa chạm tay vào mặt ...' });
+    }else{
+      console.log('Not touch');
+    }
+
+    await sleep(200);
+    run();
+  }
+  const sleep = (ms=0) => {
+    return new Promise (resolve =>setTimeout(resolve,ms));
+  }
+
+  useEffect(()=> {
+    init();
+    sound.on('end',function(){
+      canPlayAudio.current=true;
+    })
+    return ()=> {
+
+    }
+  }, []);
+
+  return (
+    <div className="main">
+      <video
+        ref={video}
+        className="video"
+        autoPlay
+      />
+
+      <div className="control">
+        <button className="btn" onClick={()=> train(NOT_TOUCH_LABEL)}>Train 1</button>
+        <button className="btn" onClick={()=> train(TOUCHED_LABEL)}>Train 2</button>
+        <button className="btn" onClick={()=> run()}>Run</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
